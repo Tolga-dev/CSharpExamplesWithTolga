065111ce3f9d7c79854cf2d45d6c2608c5cc6854
Tolga-dev <orhan.konat.12@gmail.com>
2023-08-28 22:59:24 +0300

    Startinng TensorFlow

---
+++ TensorFlowDeepMachineLearning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static Tensorflow.Binding;
+
+namespace TensorFlowNET.Examples
+{
+    /// <summary>
+    /// Simple hello world using TensorFlow
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = tf.constant("Hello, TensorFlow!");
+            Console.WriteLine(hello);
+        }
+    }
+}
