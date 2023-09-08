6a62b3cf3a653843e9e1273d804c0292b53c03eb
Tolga-dev <orhan.konat.12@gmail.com>
2023-09-08 11:55:24 +0300

    Starting New

--- NetworkInUnity/Program.cs
+++ NetworkInUnity/Program.cs
@@ -8,8 +8,9 @@ namespace NetworkInUnity
         {
             Console.Title = "Server";
             
-            Server.Start(50, 26950);
-
+            
+            
+            
             Console.ReadKey();
             
         }
