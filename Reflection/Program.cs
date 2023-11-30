accbeeb87bc54a75bcce4c773c1fa1612a37ceab
Tolga-dev <orhan.konat.12@gmail.com>
2023-11-30 09:16:00 +0300

    reflection

---
+++ Reflection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Reflection;
+ 
+
+namespace Reflection
+{
+    internal class Reflection01
+    {
+        private void Ex1()
+        {
+            int i = 42;
+            System.Type type = i.GetType();
+            Console.WriteLine(type);
+        }
+
+        private void Ex2()
+        {
+        }
+        public void Runner()
+        {
+            Ex1();
+        }
+
+    }
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Reflection.Reflection01 reflection01 = new Reflection01();
+        reflection01.Runner();
+    }
+}
---
+++ Reflection/readme.md
@@ -0,0 +1,22 @@
+Reflection
+
+to write code that can read, write code in runtime
+
+it is a ability that some programming language have to inspect their own constructs dynamically
+
+* we can load a class dynamically from an assembly
+
+basics, hierarchy of its programming constructs
+* Assemblies contain modules
+* Modules contain types
+* Types contain members
+
+it is needed when you want to determine the content of an assembly.
+
+reflection inspects the assembly and shows the metadata of that assembly
+
+
+SOURCES
+https://stackify.com/what-is-c-reflection/
+
+https://dotnettutorials.net/lesson/reflection-in-csharp/
